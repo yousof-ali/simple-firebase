9b457b49a5f708fcaf11eaae4d7d0b7e16b4fc47
Yousof Ali <yousofali293@gmail.com>
2024-07-05 00:59:24 +0600

    github login done

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,33 +1,53 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider, GithubAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { useState } from "react";
 
 const Login = () => {
     const auth = getAuth(app)
-    const provider = new GoogleAuthProvider();
-    const [user,setUser]=useState(null)
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider()
+    const [user, setUser] = useState(null)
 
     const handleGoogleSingIn = () => {
-        signInWithPopup(auth,provider)
-        .then(result => {
-            const loginUser = result.user;
-            setUser(loginUser);
-            
-        })
-        .catch(error => {
-            console.log("Error",error.massage);
-        })
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const loginUser = result.user;
+                setUser(loginUser);
+
+            })
+            .catch(error => {
+                console.log("Error", error.massage);
+            })
     }
-    const handleGoogleSingOut =()=>{
+    const handleGoogleSingOut = () => {
         signOut(auth);
         setUser(null)
+    }
+    const handleGithubSingIn = ()=>{
+        signInWithPopup(auth,githubProvider)
+        .then(result=>{
+            const githubUser = result.user;
+            console.log(githubUser)
+            setUser(githubUser);
+        })
+        .catch(error=>{
+            console.log("error",error)
+        })
+
     }
     return (
         <div className="container mx-auto my-8">
             <div className="">
-                {user?<button className="bg-gray-200 p-2" onClick={handleGoogleSingOut}>Sing Out</button>:<button className="bg-gray-200 p-2" onClick={handleGoogleSingIn}>Log in with Google</button>}
-                
-                {user&&<div>
+                {user ?
+                    <button className="bg-gray-200 p-2" onClick={handleGoogleSingOut}>Sing Out</button> :
+                    <div>
+                        <button className="bg-gray-200 p-2 mr-4" onClick={handleGoogleSingIn}>Log in with Google</button>
+                        <button className="bg-gray-200 p-2" onClick={handleGithubSingIn}>Log in with GitHub</button>
+                    </div>
+
+                }
+
+                {user && <div>
                     <h2>User Name = {user.displayName}</h2>
                     <p>User Email = {user.email}</p>
                     <img src={user.photoURL} alt="user" />
